3fd45aa3a2948d7a9852b32cc4997826c2af65e0
HR0N <vivante.des@gmail.com>
2022-06-25 21:04:24 +0300

    ok, i think i get. i cant store token in cookie as default

--- back/config/cors.php
+++ back/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://fuck-you-laravel.anakim.space'],
 
     'allowed_origins_patterns' => [],
 
--- back/routes/api.php
+++ back/routes/api.php
@@ -5,10 +5,11 @@ use App\Http\Controllers\NeilArmstrongController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
-//header('Access-Control-Allow-Origin: anakim.space');
+// it doesn't work for me.
+/*header('Access-Control-Allow-Origin', "fuck-you-laravel.anakim.space");
+header('Access-Control-Allow-Credentials', true);
 header('Access-Control-Allow-Methods: GET, POST, DELETE, PUT, PATCH, OPTIONS');
-header("Access-Control-Allow-Headers: X-Requested-With");
+header("Access-Control-Allow-Headers", "X-Requested-With");*/
 
 /*
 |--------------------------------------------------------------------------
